44ad9dfe5296eaa701575441691d1e1b65be1337
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-21 08:19:38 +0530

    [Sangmeshwar] Add: Load State Code Info from CSV Using Test Cases

---
+++ StateCensus/StateCensusTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace StateCensusTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
