f763fa1a26ee79de4a93994ee5ab2fb100d71b73
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-03-21 08:05:36 +0530

    [Sangmeshwar] Add: Load the Indian States Census Data CSV File

---
+++ StateCensus/StateCensus/CSVStateCensus.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensus
+{
+    public class CSVStateCensus
+    {
+        public static void ReadStateCensusData(String stateDataFilePath)
+        {
+            using (var reader = new StreamReader(stateDataFilePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusAnalyzerModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.State} {record.Population}{record.AreaInSqKm}{record.DensityPerSqKm}");
+                    }
+                }
+            }
+        }
+        public static void ReadStateCensusCode(String stateCodeFilePath)
+        {
+            using (var reader = new StreamReader(stateCodeFilePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusCode>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.SrNo} {record.State}{record.TIN}{record.StateCode}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StateCensus/StateCensus/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StateCensus
+{
+    public class Program
+    {
+        public static string stateDataFilePath = "C:\\Users\\hp\\source\\repos\\StateCensus\\StateCensus\\Files\\IndiaStateCensusData.csv";
+        public static string stateCodeFilePath = "C:\\Users\\hp\\source\\repos\\StateCensus\\StateCensus\\Files\\IndiaStateCode.csv";
+        public CSVStateCensus csvStateCensus = new CSVStateCensus();
+        public static void Main(string[] args)
+        {
+            CSVStateCensus.ReadStateCensusData(stateDataFilePath);
+
+        }
+    }
+}
